4835d25bc020ba2df69b60d4399a4579164f7f63
yanghcc <yanghucheng@021.com>
2019-08-16 18:13:20 +0800

    设置代理

--- build/webpack.client.dev.config.js
+++ build/webpack.client.dev.config.js
@@ -3,22 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const utils = require('./utils')
 
 module.exports = {
-    devtool: '#source-map',
-    module: {
-
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            names: ["vendor"]
-        }),
-        new HtmlWebpackPlugin({
-            chunks: [
-                'vendor',
-                'admin'
-            ],
-            filename: 'admin.html',
-            template: 'client/template/admin.html',
-            inject: true,
-        })
-    ]
+  devtool: '#source-map',
+  devServer: {
+      proxy: {
+        '/manage': {
+          target: 'https://www.html-js.cn/', // 接口域名manage
+          secure: false, // 如果是https接口，需要配置这个参数
+          changeOrigin: true, //是否跨域
+          pathRewrite: {
+            '^/manage': '' //需要rewrite的,
+          }
+        }
+      }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor']
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['vendor', 'admin'],
+      filename: 'admin.html',
+      template: 'client/template/admin.html',
+      inject: true
+    })
+  ]
 }
