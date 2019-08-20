a4599ec04a554fabcb1c1475891ef947b13e73c7
yanghcc <yanghucheng@021.com>
2019-08-20 16:57:38 +0800

    简化代码

--- server/lib/service/index.js
+++ server/lib/service/index.js
@@ -1,22 +1,14 @@
-/*
- * @Author: doramart 
- * @Date: 2019-07-16 10:26:45 
- * @Last Modified by: doramart
- * @Last Modified time: 2019-07-16 10:33:51
- */
+const fs = require('fs')
+const path = require('path')
+const modelsPath = path.resolve(__dirname, './')
 
-const fs = require('fs');
-const path = require('path');
-var modelsPath = path.resolve(__dirname, './');
-fs.readdirSync(modelsPath).forEach(function (name) {
-    if (path.extname(name) !== '') {
-        name = path.basename(name, '.js');
-        if (name != 'index') {
-            let currentName = name + 'Service';
-            exports[currentName] = require(path.resolve(modelsPath, name));
-        }
+// 将service目录所有文件当作模块暴露出去
+fs.readdirSync(modelsPath).forEach(name => {
+  if (path.extname(name) !== '') {
+    name = path.basename(name, '.js')
+    if (name != 'index') {
+      let currentName = name + 'Service' // 模块名以Service结尾
+      exports[currentName] = require(path.resolve(modelsPath, name))
     }
-});
-
-
-//DoraServiceEnd
+  }
+})
--- server/middleware/jwtCheck.js
+++ server/middleware/jwtCheck.js
@@ -1,25 +1,22 @@
-const jwt = require('jsonwebtoken');
-let settings = require('@configs/settings');
+const jwt = require('jsonwebtoken')
+let { encrypt_key } = require('@configs/settings')
 
-var token = {
-
-    checkToken: function (token) {
-        return new Promise((resolve, reject) => {
-            jwt.verify(token, settings.encrypt_key, function (err, decoded) {
-                if (err) {
-                    console.log('check token failed', err);
-                    if (err.message == "jwt expired") {
-                        resolve('0'); // 超时
-                    } else {
-                        resolve('-1'); // token非法
-                    }
-                } else {
-                    // console.log('---decoded---', decoded)
-                    resolve(decoded);
-                }
-            });
-        })
-    }
-
+module.exports = {
+  checkToken: token => {
+    return new Promise(resolve => {
+      jwt.verify(token, encrypt_key, (err, decoded) => {
+        if (err) {
+          console.log('check token failed', err)
+          if (err.message == 'jwt expired') {
+            resolve('0') // 超时
+          } else {
+            resolve('-1') // token非法
+          }
+        } else {
+          // console.log('---decoded---', decoded)
+          resolve(decoded)
+        }
+      })
+    })
+  }
 }
-module.exports = token;
